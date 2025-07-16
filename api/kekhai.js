90b8bba965f89bdf1438e0e34902a58c061b9643
admin.hatinh <admin.hatinh@vss.local>
2025-07-16 21:13:26 +0700

    commit

--- api/kekhai.js
+++ api/kekhai.js
@@ -1978,7 +1978,7 @@ router.get("/kykekhai-search-hoso", async (req, res) => {
       hoten,
       tendaily,
       maloaihinh,
-      trangthaihs,
+      // trangthaihs,
       page = 1,
       limit = 30,
     } = req.query;
@@ -2002,18 +2002,18 @@ router.get("/kykekhai-search-hoso", async (req, res) => {
     // Thêm các điều kiện tìm kiếm nếu có
     // console.log(trangthaihs);
     
-    if (trangthaihs) {
-      if(trangthaihs == 'dapheduyet'){
-        query += " AND trangthai = 0 and status_naptien=1";
-        queryCount += " AND trangthai = 0 and status_naptien=1";
-      }else if(trangthaihs == 'dahuyduyet'){
-        query += " AND trangthai = 1";
-        queryCount += " AND trangthai = 1";
-      }else if(trangthaihs == 'chuapheduyet'){
-        query += " AND trangthai = 0 and status_naptien=0";
-        queryCount += " AND trangthai = 0 and status_naptien=0";
-      }
-    }
+    // if (trangthaihs) {
+    //   if(trangthaihs == 'dapheduyet'){
+    //     query += " AND trangthai = 0 and status_naptien=1";
+    //     queryCount += " AND trangthai = 0 and status_naptien=1";
+    //   }else if(trangthaihs == 'dahuyduyet'){
+    //     query += " AND trangthai = 1";
+    //     queryCount += " AND trangthai = 1";
+    //   }else if(trangthaihs == 'chuapheduyet'){
+    //     query += " AND trangthai = 0 and status_naptien=0";
+    //     queryCount += " AND trangthai = 0 and status_naptien=0";
+    //   }
+    // }
 
     if (kykekhai) {
       query += " AND kykekhai = @kykekhai";
@@ -2142,7 +2142,7 @@ router.get("/kykekhai-search-hoso-diemthu", async (req, res) => {
       hoten,
       madaily,
       cccd,
-      trangthaihs,
+      // trangthaihs,
       page = 1,
       limit = 30,
     } = req.query;
@@ -2165,18 +2165,18 @@ router.get("/kykekhai-search-hoso-diemthu", async (req, res) => {
       `SELECT COUNT(*) AS totalCount FROM kekhai WHERE RIGHT(sohoso, 12) = '${cccd}'`;
 
     // Thêm các điều kiện tìm kiếm nếu có
-    if (trangthaihs) {
-      if(trangthaihs == 'dapheduyet'){
-        query += " AND trangthai = 0 and status_naptien=1";
-        queryCount += " AND trangthai = 0 and status_naptien=1";
-      }else if(trangthaihs == 'dahuyduyet'){
-        query += " AND trangthai = 1";
-        queryCount += " AND trangthai = 1";
-      }else if(trangthaihs == 'chuapheduyet'){
-        query += " AND trangthai = 0 and status_naptien=0";
-        queryCount += " AND trangthai = 0 and status_naptien=0";
-      }
-    }
+    // if (trangthaihs) {
+    //   if(trangthaihs == 'dapheduyet'){
+    //     query += " AND trangthai = 0 and status_naptien=1";
+    //     queryCount += " AND trangthai = 0 and status_naptien=1";
+    //   }else if(trangthaihs == 'dahuyduyet'){
+    //     query += " AND trangthai = 1";
+    //     queryCount += " AND trangthai = 1";
+    //   }else if(trangthaihs == 'chuapheduyet'){
+    //     query += " AND trangthai = 0 and status_naptien=0";
+    //     queryCount += " AND trangthai = 0 and status_naptien=0";
+    //   }
+    // }
 
     if (kykekhai) {
       query += " AND kykekhai = @kykekhai";
